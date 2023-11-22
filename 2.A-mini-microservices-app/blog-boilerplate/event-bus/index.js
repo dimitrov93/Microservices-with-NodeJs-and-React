2042403efbf7ae5eeac33b97b36b482ecec7a314
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-22 21:31:38 +0200

    complete microservices app with event-buss, query and moderation

---
+++ 2.A-mini-microservices-app/blog-boilerplate/event-bus/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const events = [];
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  events.push(event)
+
+  // Posts
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // Comments
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // Query
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  // MODERATION
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  // Event bus --- 4005
+  res.send({ status: "OK" });
+});
+
+app.get('/events', (req,res) => {
+  res.send(events)
+})
+
+app.listen(4005, () => {
+  console.log("Event bus listening on 4005");
+});
