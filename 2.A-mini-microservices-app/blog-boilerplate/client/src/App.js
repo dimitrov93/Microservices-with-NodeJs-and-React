f7fa8d7eaa42bc1e1572767710cc40ef0269348e
dimitrov93 <ts.dimitrov93@gmail.com>
2023-11-30 23:06:38 +0200

    fully completed app with docker,k8s,skaffold

--- 2.A-mini-microservices-app/blog-boilerplate/client/src/App.js
+++ 2.A-mini-microservices-app/blog-boilerplate/client/src/App.js
@@ -5,7 +5,7 @@ import PostList from "./PostList";
 const App = () => {
   return (
     <div className="container">
-      <h1>Create post</h1>
+      <h1>Create post!</h1>
       <PostCreate />
       <hr />
       <h1>Posts</h1>
