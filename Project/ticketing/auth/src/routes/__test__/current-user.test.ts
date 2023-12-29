10f7749f6e2a51bd371601d0ed62f4cc59bcd257
dimitrov93 <ts.dimitrov93@gmail.com>
2023-12-29 12:37:16 +0200

    fixed test

--- Project/ticketing/auth/src/routes/__test__/current-user.test.ts
+++ Project/ticketing/auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it("responds with details about the current user", async () => {
     .get("/api/users/currentUser")
     .set("Cookie", cookie)
     .send()
-    .expect(401);
+    .expect(200);
 
   expect(res.body.currentUser.email).toEqual("ceko@abv.bg");
 });
